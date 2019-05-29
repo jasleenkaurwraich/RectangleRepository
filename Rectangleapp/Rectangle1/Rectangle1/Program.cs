ff5a9827f8d94b41082bbfc3c97e52715604040e
jasleenkaurwraich <50706370+jasleenkaurwraich@users.noreply.github.com>
2019-05-29 01:29:36 -0400

    Add files via upload

--- Rectangleapp/Rectangle1/Rectangle1/Program.cs
+++ Rectangleapp/Rectangle1/Rectangle1/Program.cs
@@ -8,8 +8,113 @@ namespace Rectangle1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int ValidateMenuSelection()
         {
+            string userInput = "";
+            bool validMenuSelect = false;
+            while (validMenuSelect == false)
+            {
+                Console.WriteLine("1: Get Rectangle Length");
+                Console.WriteLine("2: Change Rectangle Length");
+                Console.WriteLine("3: Get Rectangle Width");
+                Console.WriteLine("4: Change Rectangle Width");
+                Console.WriteLine("5: Get Rectangle Perimeter");
+                Console.WriteLine("6: Get Rectangle Area");
+                Console.WriteLine("7: Exit\n");
+                Console.WriteLine("please select an option, by entering a number:");
+                userInput = Console.ReadLine();
+                if (userInput != "1" &&
+                   userInput != "2" &&
+                   userInput != "3" &&
+                   userInput != "4" &&
+                   userInput != "5" &&
+                   userInput != "6" &&
+                   userInput != "7")
+                {
+                    Console.WriteLine("This is not a valid menu option, please try again:\n");
+                }
+                else
+                {
+                    validMenuSelect = true;
+                }
+            }
+            Console.WriteLine();
+            return int.Parse(userInput);
+        }
+        public static int ValidateUserInput(string rectside)
+        {
+            int number = 1;
+            bool isValid = false;
+            while (isValid == false)
+            {
+                Console.WriteLine($" please enter the {rectside} of the rectangle:");
+                string userInput = Console.ReadLine();
+                Console.WriteLine();
+                bool result = int.TryParse(userInput, out number);
+                if (result == false)
+                {
+                    Console.WriteLine("That is not a valid input, please try again:\n");
+                }
+                else if (number < 0)
+                {
+                    Console.WriteLine("Number cannot be less than zero, please try again:\n");
+                }
+                else
+                {
+                    isValid = true;
+                    Console.WriteLine($"The {rectside} of your rectangle is now:{number}\n");
+                }
+            }
+            return number;
+        }
+        public static void Rectpro(int option, ref Rectangle r)
+        {
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine(">>Length of the rectangle is " + r.GetLength());
+                  
+                    break;
+                case 2:
+                    Console.WriteLine(">>New length of the rectangle is " + r.SetLength(ValidateUserInput("length")));
+                  
+                    break;
+                case 3:
+                    Console.WriteLine(">>Width of the rectangle is " + r.GetWidth());
+                    
+                    break;
+                case 4:
+                    Console.WriteLine(">>New width of the rectangle is " + r.SetWidth(ValidateUserInput("width")));
+                  
+                    break;
+                case 5:
+                    Console.WriteLine(">>Rectangle perimeter is " + r.GetPerimeter());
+                   
+                    break;
+                case 6:
+                    Console.WriteLine(">>Area of the rectangle is " + r.GetArea());
+                    
+                    break;
+                case 7:
+                    Environment.Exit(0);
+                    break;
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            rect.SetWidth(ValidateUserInput("width"));
+            rect.SetLength(ValidateUserInput("length"));
+            int Menu = 0;
+            while (Menu != 7)
+            {
+                Menu = ValidateMenuSelection();
+                Console.WriteLine("Menu  Option selected is  :: " + Menu);
+                Rectpro(Menu, ref rect);
+            }
         }
     }
 }
+
+   
