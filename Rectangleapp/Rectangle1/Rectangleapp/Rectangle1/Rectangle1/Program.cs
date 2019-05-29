d5cd5e3e9020b03650577fca34182295c2be54e9
jasleenkaurwraich <50706370+jasleenkaurwraich@users.noreply.github.com>
2019-05-29 00:00:55 -0400

    upload all the files
    All the files are uploaded of complete solution and test cases

---
+++ Rectangleapp/Rectangle1/Rectangleapp/Rectangle1/Rectangle1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle1
+{
+    class Program
+    {
+        public static int ValidateMenuSelection()
+        {
+            string userInput = "";
+            bool validMenuSelect = false;
+            while (validMenuSelect == false)
+            {
+                Console.WriteLine("1: Get Rectangle Length");
+                Console.WriteLine("2: Change Rectangle Length");
+                Console.WriteLine("3: Get Rectangle Width");
+                Console.WriteLine("4: Change Rectangle Width");
+                Console.WriteLine("5: Get Rectangle Perimeter");
+                Console.WriteLine("6: Get Rectangle Area");
+                Console.WriteLine("7: Exit\n");
+                Console.WriteLine("please select an option, by entering a number:");
+                userInput = Console.ReadLine();
+                if (userInput != "1" &&
+                   userInput != "2" &&
+                   userInput != "3" &&
+                   userInput != "4" &&
+                   userInput != "5" &&
+                   userInput != "6" &&
+                   userInput != "7")
+                {
+                    Console.WriteLine("This is not a valid menu option, please try again:\n");
+                }
+                else
+                {
+                    validMenuSelect = true;
+                }
+            }
+            Console.WriteLine();
+            return int.Parse(userInput);
+        }
+        public static int ValidateUserInput(string rectside)
+        {
+            int number = 1;
+            bool isValid = false;
+            while (isValid == false)
+            {
+                Console.WriteLine($" please enter the {rectside} of the rectangle:");
+                string userInput = Console.ReadLine();
+                Console.WriteLine();
+                bool result = int.TryParse(userInput, out number);
+                if (result == false)
+                {
+                    Console.WriteLine("That is not a valid input, please try again:\n");
+                }
+                else if (number < 0)
+                {
+                    Console.WriteLine("Number cannot be less than zero, please try again:\n");
+                }
+                else
+                {
+                    isValid = true;
+                    Console.WriteLine($"The {rectside} of your rectangle is now:{number}\n");
+                }
+            }
+            return number;
+        }
+        public static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.SetWidth(ValidateUserInput("length"));
+            r.SetLength(ValidateUserInput("width"));
+
+            ValidateMenuSelection();
+        }
+    }
+}
---
+++ Rectangleapp/Rectangle1/Rectangleapp/Rectangle1/Rectangle1nunittests/Class1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rectangle1;
+using NUnit.Framework;
+
+
+namespace Rectangle1nunittests
+{
+    [TestFixture]
+     class RectTeatCases
+    {
+        [Test]
+        //Test for GetLength
+        public void GetLength_input8_expectedLengthEquals8()
+        {
+
+            int l = 8;
+            int w = 4;
+            Rectangle testRec = new Rectangle(l,w);
+            int Length = testRec.GetLength();
+            Assert.AreEqual(l, Length);
+
+
+
+
+        }
+
+        [Test]
+        //Test for SetLength
+        public void SetLength_input4_expectedLengthEquals4()
+        {
+
+            int l = 4;
+            int w = 2;
+            Rectangle testRec = new Rectangle(l, w);
+            int Length = testRec.SetLength(l);
+            Assert.AreEqual(l, Length);
+
+
+
+
+        }
+
+        [Test]
+        //Test for GetWidth
+        public void GetWidth_input2_expectedWidthEquals2()
+        {
+
+            int l = 2;
+            int w = 6;
+            Rectangle testRec = new Rectangle(l, w);
+            int Width = testRec.GetWidth();
+            Assert.AreEqual(w, Width);
+
+
+
+
+        }
+
+        [Test]
+        //Test for SetWidth
+        public void SetWidth_input9_expectedWidthEquals9()
+        {
+
+            int l = 4;
+            int w = 2;
+            Rectangle testRec = new Rectangle(l, w);
+            int Width = testRec.SetWidth(w);
+            Assert.AreEqual(w, Width);
+
+
+
+
+        }
+
+
+        [Test]
+        public void Area_inputLength2Width4_expectedAreaEquals8()
+        {
+
+            int l = 7;
+            int w = 8;
+            int expectedResult = l * w;
+            Rectangle testRec = new Rectangle(l, w);
+            int actualResult = testRec.GetArea();
+            Assert.AreEqual(expectedResult, actualResult);
+
+
+
+
+        }
+
+        [Test]
+        public void Perimeter_inputLength8Width3_expectedPerimeterEquals8()
+        {
+
+            int l = 8;
+            int w = 3;
+            int expectedResult = (l*2)+(w*2);
+            Rectangle testRec = new Rectangle(l, w);
+            int actualResult = testRec.GetPerimeter();
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+
+
+
+
+    }
+
+
+}
